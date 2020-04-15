fe4d605321009a3bf63ff3c2b1abdb9bfb253f58
Mambisi Zempare <lilbizi96@gmail.com>
2020-04-15 00:11:05 +0000

    Fix linux path

--- src/bin/server/file_dirs.rs
+++ src/bin/server/file_dirs.rs
@@ -4,20 +4,6 @@ use crate::APP_INFO;
 use std::ffi::OsStr;
 
 pub fn config_file_path() -> Option<PathBuf> {
-    if cfg!(target_os = "linux") {
-        let mut path = PathBuf::new();
-        path.push("usr");
-        path.push(".conf");
-        path.push("escanor");
-        path.push("config");
-        path.set_extension("yaml");
-
-        let mut  directory  = PathBuf::from("/usr/.conf/escanor");
-        if !directory.exists() {
-            std::fs::create_dir_all(directory);
-        }
-        return Some(path);
-    }
     let p = match create_file_path(AppDataType::UserConfig, "config", "yaml") {
         None => { return None; }
         Some(p) => { p }
@@ -26,21 +12,6 @@ pub fn config_file_path() -> Option<PathBuf> {
 }
 
 pub fn db_file_path() -> Option<PathBuf> {
-    if cfg!(target_os = "linux") {
-        let mut path = PathBuf::new();
-        path.push("..");
-        path.push("usr");
-        path.push("lib");
-        path.push("escanor");
-        path.push("dump");
-        path.set_extension("esbd");
-
-        let mut  directory  = PathBuf::from("/usr/lib/escanor");
-        if !directory.exists() {
-            std::fs::create_dir_all(directory);
-        }
-        return Some(path);
-    }
 
     let p = match create_file_path(AppDataType::UserCache, "dump", "esdb") {
         None => { return None; }
